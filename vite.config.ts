d98854f246a5193f9fa2c04d9e044971e2ba47e9
Stas <stanislav.s.hlushko@gmail.com>
2023-11-03 15:53:24 +0200

    fix alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import path from 'path'
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+		alias: [
+			{ find: '@', replacement: path.resolve(__dirname, 'src/') },
+			{ find: '@pages', replacement: path.resolve(__dirname, 'src/pages/') },
+			{ find: '@styles', replacement: path.resolve(__dirname, 'src/styles/') },
+			{ find: '@components', replacement: path.resolve(__dirname, 'src/components/') },
+			{ find: '@ui', replacement: path.resolve(__dirname, 'src/ui/') },
+		],
 	},
 })
